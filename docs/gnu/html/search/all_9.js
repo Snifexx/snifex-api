95f13a43328ce05212de47c5695cc5089a6da504
matteoserver <matteobonesolii@gmail.com>
2025-03-24 02:13:16 +0100

    Continuing documentation....

--- docs/gnu/html/search/all_9.js
+++ docs/gnu/html/search/all_9.js
@@ -1,11 +1,13 @@
 var searchData=
 [
   ['vec_0',['Vec',['../group__vector.html#ga21ec81b33f2746d76b94b77c7f990379',1,'snifex-api.h']]],
-  ['vec_5fcreate_1',['vec_create',['../group__vector.html#ga85377c64c46343c4dbf05a73724db8a6',1,'snifex-api.h']]],
-  ['vec_5ffrom_2',['vec_from',['../group__vector.html#ga98ce1bacc1d60cd65f2c0ba9f61840d3',1,'snifex-api.h']]],
-  ['vec_5fidx_3',['vec_idx',['../group__vector.html#gaa85f3533c2232edfd7fcc899bd5f6378',1,'snifex-api.h']]],
-  ['vec_5flast_4',['vec_last',['../group__vector.html#ga8658b8a80e25d259520ed4cd4adf1729',1,'snifex-api.h']]],
-  ['vec_5fpop_5',['vec_pop',['../group__vector.html#ga58a40fa5257bab64d1fd6f7046d30e0e',1,'snifex-api.h']]],
-  ['vec_5fpush_6',['vec_push',['../group__vector.html#gacf1c58bf560b4b89d60091e093bc5d88',1,'snifex-api.h']]],
-  ['vector_7',['Vector',['../group__vector.html',1,'']]]
+  ['vec_5fappend_1',['vec_append',['../group__vector.html#ga06b74abb56b0226f908d1157353176e3',1,'snifex-api.h']]],
+  ['vec_5fcreate_2',['vec_create',['../group__vector.html#ga85377c64c46343c4dbf05a73724db8a6',1,'snifex-api.h']]],
+  ['vec_5ffrom_3',['vec_from',['../group__vector.html#ga98ce1bacc1d60cd65f2c0ba9f61840d3',1,'snifex-api.h']]],
+  ['vec_5fidx_4',['vec_idx',['../group__vector.html#gaa85f3533c2232edfd7fcc899bd5f6378',1,'snifex-api.h']]],
+  ['vec_5flast_5',['vec_last',['../group__vector.html#ga8658b8a80e25d259520ed4cd4adf1729',1,'snifex-api.h']]],
+  ['vec_5fpop_6',['vec_pop',['../group__vector.html#ga58a40fa5257bab64d1fd6f7046d30e0e',1,'snifex-api.h']]],
+  ['vec_5fpush_7',['vec_push',['../group__vector.html#gacf1c58bf560b4b89d60091e093bc5d88',1,'snifex-api.h']]],
+  ['vec_5fswap_5fremove_8',['vec_swap_remove',['../group__vector.html#ga944fa232b956cf6561b3fc242da0d200',1,'snifex-api.h']]],
+  ['vector_9',['Vector',['../group__vector.html',1,'']]]
 ];
--- docs/nongnu/html/group__vector.js
+++ docs/nongnu/html/group__vector.js
@@ -2,10 +2,12 @@ var group__vector =
 [
     [ "DefineVec", "group__vector.html#gac69846d6bf7de13c7eb8cdf3351a92ef", null ],
     [ "Vec", "group__vector.html#ga21ec81b33f2746d76b94b77c7f990379", null ],
+    [ "vec_append", "group__vector.html#ga06b74abb56b0226f908d1157353176e3", null ],
     [ "vec_create", "group__vector.html#ga85377c64c46343c4dbf05a73724db8a6", null ],
     [ "vec_from", "group__vector.html#ga98ce1bacc1d60cd65f2c0ba9f61840d3", null ],
     [ "vec_idx", "group__vector.html#gaa85f3533c2232edfd7fcc899bd5f6378", null ],
     [ "vec_last", "group__vector.html#ga8658b8a80e25d259520ed4cd4adf1729", null ],
     [ "vec_pop", "group__vector.html#ga58a40fa5257bab64d1fd6f7046d30e0e", null ],
-    [ "vec_push", "group__vector.html#gacf1c58bf560b4b89d60091e093bc5d88", null ]
+    [ "vec_push", "group__vector.html#gacf1c58bf560b4b89d60091e093bc5d88", null ],
+    [ "vec_swap_remove", "group__vector.html#ga944fa232b956cf6561b3fc242da0d200", null ]
 ];
