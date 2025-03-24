1941f0c37388faed50de376fe2c83a2859db1204
matteoserver <matteobonesolii@gmail.com>
2025-03-24 01:49:32 +0100

    Continuing documentation...

--- docs/nongnu/html/search/all_9.js
+++ docs/nongnu/html/search/all_9.js
@@ -3,5 +3,9 @@ var searchData=
   ['vec_0',['Vec',['../group__vector.html#ga21ec81b33f2746d76b94b77c7f990379',1,'snifex-api.h']]],
   ['vec_5fcreate_1',['vec_create',['../group__vector.html#ga85377c64c46343c4dbf05a73724db8a6',1,'snifex-api.h']]],
   ['vec_5ffrom_2',['vec_from',['../group__vector.html#ga98ce1bacc1d60cd65f2c0ba9f61840d3',1,'snifex-api.h']]],
-  ['vector_3',['Vector',['../group__vector.html',1,'']]]
+  ['vec_5fidx_3',['vec_idx',['../group__vector.html#gaa85f3533c2232edfd7fcc899bd5f6378',1,'snifex-api.h']]],
+  ['vec_5flast_4',['vec_last',['../group__vector.html#ga8658b8a80e25d259520ed4cd4adf1729',1,'snifex-api.h']]],
+  ['vec_5fpop_5',['vec_pop',['../group__vector.html#ga58a40fa5257bab64d1fd6f7046d30e0e',1,'snifex-api.h']]],
+  ['vec_5fpush_6',['vec_push',['../group__vector.html#gacf1c58bf560b4b89d60091e093bc5d88',1,'snifex-api.h']]],
+  ['vector_7',['Vector',['../group__vector.html',1,'']]]
 ];
